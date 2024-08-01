c9595f88c34b94cdf62744432be70ece315b2480
78 mmd 69 <mmdqurani@gmail.com>
2024-08-01 11:06:57 +0330

    Changing the style of the modal box

--- js/app.js
+++ js/app.js
@@ -4,23 +4,30 @@ let imgBox = $.querySelectorAll ('.img_one');
 let imgMainModal = $.getElementById('img_main_modal');
 let imgLastModal = $.querySelectorAll('.img_last');
 let imgBoxLastModal = $.querySelector('.box_img_last_modal');
+let textBoxImgModal = $.querySelector('.text_box_img_modal');
 
 let imgTrandingList = {
     one : [
         {id : 1 , srcImg : '../img/img-trending/Primary Photo Placeholder.png'},
-        {id : 1 , srcImg : '../img/img-trending/Secondary Photo Placeholder.png'},
-        {id : 1 , srcImg : '../img/img-trending/Secondary Photo Placeholder-1.png'},
+        {id : 2 , srcImg : '../img/img-trending/Secondary Photo Placeholder.png'},
+        {id : 3 , srcImg : '../img/img-trending/Secondary Photo Placeholder-1.png'},
     ],
     two : [
-        {id : 1 , srcImg : '../img/img-trending/Primary Photo Placeholder-1.png'},
-        {id : 1 , srcImg : '../img/img-trending/Secondary Photo Placeholder-2.png'},
-        {id : 1 , srcImg : '../img/img-trending/Secondary Photo Placeholder-3.png'},
+        {id : 4 , srcImg : '../img/img-trending/Primary Photo Placeholder-1.png'},
+        {id : 5 , srcImg : '../img/img-trending/Secondary Photo Placeholder-2.png'},
+        {id : 6 , srcImg : '../img/img-trending/Secondary Photo Placeholder-3.png'},
     ],
     three : [
-        {id : 1 , srcImg : '../img/img-trending/Primary Photo Placeholder-2.png'},
-        {id : 1 , srcImg : '../img/img-trending/Secondary Photo Placeholder-4.png'},
-        {id : 1 , srcImg : '../img/img-trending/Secondary Photo Placeholder-5.png'},
+        {id : 7 , srcImg : '../img/img-trending/Primary Photo Placeholder-2.png'},
+        {id : 8 , srcImg : '../img/img-trending/Secondary Photo Placeholder-4.png'},
+        {id : 9 , srcImg : '../img/img-trending/Secondary Photo Placeholder-5.png'},
     ],
+}
+
+let textImgTrandingList = {
+    one : {txt : 'lorem text lorem text lorem text lorem text lorem text lorem text lorem text lorem text'},
+    two : {txt : 'lorem text lorem text lorem text lorem text lorem text lorem text lorem text lorem text'},
+    three : {txt : 'lorem text lorem text lorem text lorem text lorem text lorem text lorem text lorem text'},
 }
 
 let saveSrcLastImg ;
@@ -30,9 +37,13 @@ imgBox.forEach((box) => {
             let saveSrcImgMain = imgTrandingList.one.filter((src) => {
                 return src.srcImg === '../img/img-trending/Primary Photo Placeholder.png';
             })
+
+            textBoxImgModal.innerHTML = '';
+            textBoxImgModal.insertAdjacentHTML('beforeend' , `<span>${textImgTrandingList.one.txt}</span>`);
+
             imgBoxLastModal.innerHTML = '';
             imgTrandingList.one.forEach((item) => {
-                imgBoxLastModal.insertAdjacentHTML('beforeend' , `<div class="col-6"><img class="img_last" src="${item.srcImg}" width="150" alt=""></div>`);
+                imgBoxLastModal.insertAdjacentHTML('beforeend' , `<div class=""><img class="img_last" src="${item.srcImg}" width="80" alt=""></div>`);
             })
             let lastImg = $.querySelectorAll('.img_last');
             lastImg.forEach((item) => {
@@ -47,9 +58,13 @@ imgBox.forEach((box) => {
             let saveSrcImgMain = imgTrandingList.two.filter((src) => {
                 return src.srcImg === '../img/img-trending/Primary Photo Placeholder-1.png';
             })
+
+            textBoxImgModal.innerHTML = '';
+            textBoxImgModal.insertAdjacentHTML('beforeend' , `<span>${textImgTrandingList.one.txt}</span>`);
+
             imgBoxLastModal.innerHTML = '';
             imgTrandingList.two.forEach((item) => {
-                imgBoxLastModal.insertAdjacentHTML('beforeend' , `<div class="col-6"><img class="img_last" src="${item.srcImg}" width="150" alt=""></div>`);
+                imgBoxLastModal.insertAdjacentHTML('beforeend' , `<div class=""><img class="img_last" src="${item.srcImg}" width="80" alt=""></div>`);
             })
             let lastImg = $.querySelectorAll('.img_last');
             lastImg.forEach((item) => {
@@ -64,9 +79,13 @@ imgBox.forEach((box) => {
             let saveSrcImgMain = imgTrandingList.three.filter((src) => {
                 return src.srcImg === '../img/img-trending/Primary Photo Placeholder-2.png';
             })
+
+            textBoxImgModal.innerHTML = '';
+            textBoxImgModal.insertAdjacentHTML('beforeend' , `<span>${textImgTrandingList.one.txt}</span>`);
+
             imgBoxLastModal.innerHTML = '';
             imgTrandingList.three.forEach((item) => {
-                imgBoxLastModal.insertAdjacentHTML('beforeend' , `<div class="col-6"><img class="img_last" src="${item.srcImg}" width="150" alt=""></div>`);
+                imgBoxLastModal.insertAdjacentHTML('beforeend' , `<div class=""><img class="img_last" src="${item.srcImg}" width="80" alt=""></div>`);
             })
             let lastImg = $.querySelectorAll('.img_last');
             lastImg.forEach((item) => {
