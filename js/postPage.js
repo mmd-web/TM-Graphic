e22231970068ad37dcf01040f9827a19b8d0858e
78 mmd 69 <mmdqurani@gmail.com>
2024-08-12 00:26:56 +0330

    Refactoring the JS codes of banner pages and posts

--- js/postPage.js
+++ js/postPage.js
@@ -4,8 +4,8 @@ let textTitle = document.getElementById('title');
 let textSubTitle = document.getElementById('sub_title');
 
 let imgBannerCardList = [
-    {id : 1 , srcImg : 'img/instagram-stories-template-with-online-shopping_23-2148545458.jpg', name : 'ali', content : 'lorem hello world lorem hello world lorem hello world'},
-    {id : 2 , srcImg : 'img/instagram-stories-template-with-online-shopping_23-2148545458.jpg', name : 'hello', content : 'lorem hello world lorem hello world lorem hello world'},
+    {id : 1 , srcImg : 'img/instagram-stories-template-with-online-shopping_23-2148545458.jpg', name : 'ali', content : 'salam donya'},
+    {id : 2 , srcImg : 'img/instagram-stories-template-with-online-shopping_23-2148545458.jpg', name : 'reza', content : 'lorem hello world lorem hello world lorem hello world'},
     {id : 3 , srcImg : 'img/instagram-stories-template-with-online-shopping_23-2148545458.jpg', name : 'hello', content : 'lorem hello world lorem hello world lorem hello world'},
     {id : 4 , srcImg : 'img/instagram-stories-template-with-online-shopping_23-2148545458.jpg' , name : 'hello', content : 'lorem hello world lorem hello world lorem hello world'},
     {id : 5 , srcImg : 'img/flat-design-teacher-s-day-template_23-2150254244.jpg' , name : 'hello', content : 'lorem hello world lorem hello world lorem hello world'},
@@ -30,16 +30,17 @@ let boxBannerCard ;
 imgBannerCardList.forEach((item) => {
     // console.log(item.srcImg);
     
-    bannerBoxContent.insertAdjacentHTML ('beforeend' , `<div class="col-12 col-sm-6 col-lg-3" data-aos="fade-up"><a href="#" id="banner_box_item" data-bs-toggle="modal" data-bs-target="#bannerCard" class="anime_box_item background_color_two text-decoration-none d-flex flex-column rounded-4 overflow-hidden"><div><img src="${item.srcImg}" style="width: 100% !important;" alt=""></div><div class="py-lg-3 py-xl-4 py-sm-4 py-3 container text-light fs-4"><span>${item.name}</span></div></a></div>`)
+    bannerBoxContent.insertAdjacentHTML ('beforeend' , `<div class="col-12 col-sm-6 col-lg-3" data-aos="fade-up"><a href="#" id="${item.id}" data-bs-toggle="modal" data-bs-target="#bannerCard" class="banner_box_item anime_box_item background_color_two text-decoration-none d-flex flex-column rounded-4 overflow-hidden"><div><img src="${item.srcImg}" style="width: 100% !important;" alt=""></div><div class="py-lg-3 py-xl-4 py-sm-4 py-3 container text-light fs-4"><span>${item.name}</span></div></a></div>`)
 
-    boxBannerCard = document.querySelectorAll('#banner_box_item');
+    boxBannerCard = document.querySelectorAll('.banner_box_item');
     boxBannerCard.forEach((banner) => {
         banner.addEventListener('click' , (event) => {
-            // imgVisitingCard.src = event.target.firstElementChild
             saveSrcCartBanner = banner.firstElementChild.firstElementChild.src;
             imgBannerCard.src = saveSrcCartBanner;
-            textTitle.innerHTML = item.name;
-            textSubTitle.innerHTML = item.content
+            if (+banner.id === item.id) {
+                textTitle.innerHTML = item.name;
+                textSubTitle.innerHTML = item.content;
+            }
         })
     })
 })
